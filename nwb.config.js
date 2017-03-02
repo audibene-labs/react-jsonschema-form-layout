6c89b86b8f3b0009f3e40746cbb6d0fcea936787
marsch <marsch.js@gmail.com>
2017-03-02 16:45:37 +0100

    make demo work

--- nwb.config.js
+++ nwb.config.js
@@ -10,6 +10,37 @@ module.exports = {
   webpack: {
     html: {
       template: `demo/src/index.html`
+    },
+    uglify: {
+      sourceMap: false,
+      mangle: false,
+      comments: false,
+      compress: {
+        screw_ie8: false,
+        sequences: false,
+        properties: false,
+        dead_code: false,
+        drop_debugger: true,
+        unsafe: false,
+        conditionals: false,
+        comparisons: false,
+        evaluate: false,
+        booleans: false,
+        loops: false,
+        unused: false,
+        hoist_funs: false,
+        hoist_vars: false,
+        if_return: false,
+        join_vars: false,
+        cascade: false,
+        warnings: false,
+        negate_iife: false,
+        pure_getters: false,
+        pure_funcs: false,
+        drop_console: true,
+        keep_fargs: false,
+        keep_fnames: false
+      }
     }
   }
 }
